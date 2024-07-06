366c91e898bf5ed1e962d87b31cb87270caa16f7
Stefan Claas <stefan.claas@mail.ru>
2024-07-06 10:05:16 +0200

    Add files via upload

--- nora.go
+++ nora.go
@@ -27,11 +27,12 @@ func main() {
 	passwordPtr := flag.String("p", "", "the password")
 	noncePtr := flag.Int("n", 1, "the number of nonces to be generated")
 	savePtr := flag.Bool("s", false, "save nonces to files")
+	partyBPtr := flag.Bool("b", false, "indicate if this is Party B")
 
 	flag.Parse()
 
 	if *passwordPtr == "" {
-		fmt.Println("Usage: -p <password> [-n number of nonces] [-l length of the nonce] [-s save nonces to files]")
+		fmt.Println("Usage: -p <password> [-b party B] [-n number of nonces] [-l length of the nonce] [-s save nonces]")
 		return
 	}
 
@@ -44,6 +45,16 @@ func main() {
 	password := *passwordPtr
 	date := time.Now().UTC().Format("20060102")
 	hash := sha256.Sum256([]byte(password + date))
+
+	if *partyBPtr {
+		// Increment the hash by 1 if this is Party B
+		for i := len(hash) - 1; i >= 0; i-- {
+			hash[i]++
+			if hash[i] != 0 {
+				break
+			}
+		}
+	}
 
 	// Use HKDF to derive nonces
 	hkdfReader := hkdf.New(sha256.New, hash[:], nil, nil)
@@ -65,3 +76,4 @@ func main() {
 		}
 	}
 }
+
